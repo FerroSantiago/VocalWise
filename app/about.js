5735c9f1a39f8c3aef063f36318ae2643d89770a
FerroSantiago <santiagoferroraffin@gmail.com>
2024-09-10 12:37:14 -0300

    add about

---
+++ app/about.js
@@ -0,0 +1,47 @@
+import { View, Text, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient"; // Biblioteca para el fondo degradado
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Link } from "expo-router";
+
+export default function About() {
+  const insets = useSafeAreaInsets();
+  return (
+    <LinearGradient
+      colors={["rgba(51, 102, 204, 0.8)", "#222"]}
+      style={{
+        height: "100%",
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+      }}
+    >
+      <View style={styles.container}>
+        <Link style={styles.returnButton} href="/">
+          Volver
+        </Link>
+        <Text style={styles.teamText}>
+          Este proyecto esta siendo motorizado a base de videos de MiduDev y
+          codigo de ChatGPT
+        </Text>
+      </View>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  returnButton: {
+    color: "#FFF",
+    fontSize: 20,
+    borderWidth: 2,
+    padding: 5,
+    borderRadius: 20,
+  },
+  teamText: {
+    color: "#CCC",
+    fontSize: 30,
+  },
+});
--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from "react";
 import {
   View,
   Image,
@@ -11,9 +11,10 @@ import {
   Dimensions,
   Platform,
 } from "react-native";
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Icon from "react-native-vector-icons/Feather"; // Biblioteca de iconos
 import { LinearGradient } from "expo-linear-gradient"; // Biblioteca para el fondo degradado
+import { Link } from "expo-router";
 
 import logoBlanco from "../assets/logoBlanco.png";
 
@@ -59,93 +60,107 @@ export default function Chat() {
   };
 
   const previousChats = [
-    'Final Análisis Matemático II',
-    'Mejora de muletillas',
-    'Presentación Paradigmas de Programación',
-    'Aprendiendo oratoria',
+    "Final Análisis Matemático II",
+    "Mejora de muletillas",
+    "Presentación Paradigmas de Programación",
+    "Aprendiendo oratoria",
   ];
 
   return (
     <LinearGradient
-        colors={["rgba(51, 102, 204, 0.8)", "#222"]} style={{ height: "100%", paddingTop: insets.top, paddingBottom: insets.bottom }}
-      >
-      <View style={{ height: "100%"}}>
-          {/* VocalWise Logo */}
-          <Image source={logoBlanco} style={styles.logo} resizeMode="contain" />
+      colors={["rgba(51, 102, 204, 0.8)", "#222"]}
+      style={{
+        height: "100%",
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+      }}
+    >
+      <View style={{ height: "100%" }}>
+        {/* VocalWise Logo */}
+        <Image source={logoBlanco} style={styles.logo} resizeMode="contain" />
 
-          {/* Menu icon */}
-          <TouchableOpacity onPress={toggleMenu} style={styles.menuButton}>
-            <View style={styles.menuLine} />
-            <View style={styles.menuLine} />
-            <View style={styles.menuLine} />
-          </TouchableOpacity>
+        {/* Menu icon */}
+        <TouchableOpacity onPress={toggleMenu} style={styles.menuButton}>
+          <View style={styles.menuLine} />
+          <View style={styles.menuLine} />
+          <View style={styles.menuLine} />
+        </TouchableOpacity>
 
-          {/* Background overlay with fading effect */}
-          {isMenuOpen && (
-            <Animated.View
-              style={[styles.overlay, { opacity: fadeAnim }]}
-            />
-          )}
+        {/* Background overlay with fading effect */}
+        {isMenuOpen && (
+          <Animated.View style={[styles.overlay, { opacity: fadeAnim }]} />
+        )}
 
-          {/* Sidebar Menu */}
-          {isMenuOpen && (
-            <Animated.View style={[styles.sidebarMenu, { transform: [{ translateX: slideAnim }] }]}>
-              <TouchableOpacity onPress={toggleMenu} style={styles.closeButton}>
-              </TouchableOpacity>
-              <View style={styles.sidebarContent}>
-                <Text style={styles.sidebarTitle}>Chats Anteriores</Text>
-                <ScrollView>
-                  {previousChats.map((chat, index) => (
-                    <TouchableOpacity key={index} style={styles.chatItem}>
-                      <Text style={styles.chatText}>{chat}</Text>
-                    </TouchableOpacity>
-                  ))}
-                </ScrollView>
-              </View>
-            </Animated.View>
-          )}
+        {/* Sidebar Menu */}
+        {isMenuOpen && (
+          <Animated.View
+            style={[
+              styles.sidebarMenu,
+              { transform: [{ translateX: slideAnim }] },
+            ]}
+          >
+            <TouchableOpacity
+              onPress={toggleMenu}
+              style={styles.closeButton}
+            ></TouchableOpacity>
+            <View style={styles.sidebarContent}>
+              <Text style={styles.sidebarTitle}>Chats Anteriores</Text>
+              <ScrollView>
+                {previousChats.map((chat, index) => (
+                  <TouchableOpacity key={index} style={styles.chatItem}>
+                    <Text style={styles.chatText}>{chat}</Text>
+                  </TouchableOpacity>
+                ))}
+              </ScrollView>
+            </View>
+          </Animated.View>
+        )}
 
-          {/* Chat content */}
-          <View style={styles.chatContent}>
-            {/* Example prompts */}
-            <View style={styles.prompts}>
-              {/* Right Prompt */}
-              <View style={styles.promptRight}>
-                <View style={styles.promptBox}>
-                  <Text style={styles.promptText}>Ejemplo Prompt</Text>
-                  <Text style={styles.icon}>💡</Text>
-                </View>
-              </View>
+        <Link style={styles.aboutButton} href="/about">
+          Ir al About
+        </Link>
 
-              {/* Left Prompt */}
-              <View style={styles.promptLeft}>
-                <View style={styles.promptBox}>
-                  <Text style={styles.promptText}>Ejemplo Pormpt</Text>
-                  <Text style={styles.icon}>💡</Text>
-                </View>
+        {/* Chat content */}
+        <View style={styles.chatContent}>
+          {/* Example prompts */}
+          <View style={styles.prompts}>
+            {/* Right Prompt */}
+            <View style={styles.promptRight}>
+              <View style={styles.promptBox}>
+                <Text style={styles.promptText}>Ejemplo Prompt</Text>
+                <Text style={styles.icon}>💡</Text>
               </View>
             </View>
 
-            {/* Input field */}
-            <View style={styles.inputContainer}>
-              <TextInput
-                placeholder="¿Listo para aprender?"
-                placeholderTextColor="#CCC"
-                style={styles.input}
-              />
-              <Icon
-                name="paperclip"
-                size={20}
-                color="#999"
-                style={styles.iconLeft}
-              />
-              <TouchableOpacity style={styles.sendButton}>
-                <Icon name="send" size={20} color="#999" />
-              </TouchableOpacity>
+            {/* Left Prompt */}
+            <View style={styles.promptLeft}>
+              <View style={styles.promptBox}>
+                <Text style={styles.promptText}>Ejemplo Pormpt</Text>
+                <Text style={styles.icon}>💡</Text>
+              </View>
             </View>
           </View>
+
+          {/* Input field */}
+          <View style={styles.inputContainer}>
+            <TextInput
+              placeholder="¿Listo para aprender?"
+              placeholderTextColor="#CCC"
+              style={styles.input}
+            />
+            <Icon
+              name="paperclip"
+              size={20}
+              color="#999"
+              style={styles.iconLeft}
+            />
+            <TouchableOpacity style={styles.sendButton}>
+              <Icon name="send" size={20} color="#999" />
+            </TouchableOpacity>
+          </View>
         </View>
-      </LinearGradient>
+      </View>
+    </LinearGradient>
   );
 }
 
@@ -153,38 +168,46 @@ const styles = StyleSheet.create({
   logo: {
     top: Platform.OS === "web" ? "25%" : "10%", // Posición ajustada para que quede en el fondo
     left: Platform.OS === "web" ? "50%" : "45%",
-    transform: [{ translateX: Platform.OS === "web" ? -width * 0.185 : -width * 0.35 }], // Centrado dinámico
+    transform: [
+      { translateX: Platform.OS === "web" ? -width * 0.185 : -width * 0.35 },
+    ], // Centrado dinámico
     width: Platform.OS === "web" ? width * 0.4 : width * 0.85, // Tamaño del logo
     height: Platform.OS === "web" ? height * 0.4 : height * 0.85,
     opacity: 0.1, // Transparencia del logo
   },
+  aboutButton: {
+    position: "absolute",
+    top: 16,
+    right: 16,
+    color: "#FFF"
+  },
   menuButton: {
-    position: 'absolute',
+    position: "absolute",
     top: 16,
     left: 16,
     zIndex: 10,
-    flexDirection: 'column',
-    justifyContent: 'space-between',
+    flexDirection: "column",
+    justifyContent: "space-between",
     height: 20,
-  },  
+  },
   menuLine: {
     width: 20,
     height: 2,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 2,
   },
   sidebarMenu: {
     position: "absolute",
     top: 0,
     left: 0,
-    height: '100%',
+    height: "100%",
     width: 256,
-    backgroundColor: '#333',
+    backgroundColor: "#333",
     zIndex: 5,
     padding: 16,
   },
   closeButton: {
-    position: 'absolute',
+    position: "absolute",
     top: 16,
     right: 16,
   },
@@ -192,28 +215,28 @@ const styles = StyleSheet.create({
     marginTop: 64,
   },
   sidebarTitle: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 16,
   },
   overlay: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'rgba(0, 0, 0, 0.45)', // Fondo oscuro semitransparente
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.45)", // Fondo oscuro semitransparente
     zIndex: 4,
   },
   chatItem: {
     padding: 8,
     borderRadius: 8,
-    backgroundColor: '#444',
+    backgroundColor: "#444",
     marginBottom: 8,
   },
   chatText: {
-    color: 'white',
+    color: "white",
   },
   chatContent: {
     flex: 1,
