9f6a4e78bc17547916dda9282a64e13acbb5397b
FerroSantiago <santiagoferroraffin@gmail.com>
2024-09-11 09:53:30 -0300

    fix input + fix animation

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   View,
   Image,
@@ -12,50 +12,43 @@ import {
   Platform,
 } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import Icon from "react-native-vector-icons/Feather"; // Biblioteca de iconos
-import { LinearGradient } from "expo-linear-gradient"; // Biblioteca para el fondo degradado
-import { Link } from "expo-router";
+import Icon from "react-native-vector-icons/Feather";
+import { LinearGradient } from "expo-linear-gradient";
 
 import logoBlanco from "../assets/logoBlanco.png";
 
-const { width, height } = Dimensions.get("window"); // Tamaño de la pantalla
+const { width, height } = Dimensions.get("window");
 
 export default function Chat() {
   const insets = useSafeAreaInsets();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const slideAnim = useRef(new Animated.Value(-256)).current; // Estado de animación
-  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const [isMenuOpen, setIsMenuOpen] = useState(Platform.OS === "web");
+  const slideAnim = useRef(
+    new Animated.Value(Platform.OS === "web" ? 0 : -256)
+  ).current;
+  const fadeAnim = useRef(
+    new Animated.Value(Platform.OS === "web" ? 0 : -256)
+  ).current;
+
+  useEffect(() => {
+    if (Platform.OS !== "web") {
+      // Solo aplicamos animación en dispositivos móviles
+      Animated.timing(slideAnim, {
+        toValue: isMenuOpen ? 0 : -width * 0.8, // Ancho ajustado en móviles
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+
+      Animated.timing(fadeAnim, {
+        toValue: isMenuOpen ? 1 : 0, // Opacidad para el fondo
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [isMenuOpen]);
 
   const toggleMenu = () => {
-    if (isMenuOpen) {
-      // Animar al cerrar
-      Animated.parallel([
-        Animated.timing(slideAnim, {
-          toValue: -width, // Deslizar fuera de la pantalla
-          duration: 300,
-          useNativeDriver: true,
-        }),
-        Animated.timing(fadeAnim, {
-          toValue: 0, // Hacer transparente el fondo
-          duration: 300,
-          useNativeDriver: true,
-        }),
-      ]).start(() => setIsMenuOpen(false));
-    } else {
-      setIsMenuOpen(true); // Mostrar el menú primero
-      // Animar al abrir
-      Animated.parallel([
-        Animated.timing(slideAnim, {
-          toValue: 0, // Deslizar hacia dentro de la pantalla
-          duration: 300,
-          useNativeDriver: true,
-        }),
-        Animated.timing(fadeAnim, {
-          toValue: 1, // Hacer visible el fondo
-          duration: 300,
-          useNativeDriver: true,
-        }),
-      ]).start();
+    if (Platform.OS !== "web") {
+      setIsMenuOpen(!isMenuOpen);
     }
   };
 
@@ -80,47 +73,40 @@ export default function Chat() {
         <Image source={logoBlanco} style={styles.logo} resizeMode="contain" />
 
         {/* Menu icon */}
-        <TouchableOpacity onPress={toggleMenu} style={styles.menuButton}>
-        <Icon
-              name="menu"
-              size={40}
-              color="#CCC"
-            />
-        </TouchableOpacity>
+        {Platform.OS !== "web" && (
+          <TouchableOpacity onPress={toggleMenu} style={styles.menuButton}>
+            <Icon name="menu" size={40} color="#CCC" />
+          </TouchableOpacity>
+        )}
 
         {/* Background overlay with fading effect */}
-        {isMenuOpen && (
-          <Animated.View style={[styles.overlay, { opacity: fadeAnim }]} />
-        )}
-
+        <Animated.View
+          style={[styles.overlay, { opacity: fadeAnim }]}
+          pointerEvents={isMenuOpen ? "auto" : "none"} // Control de la interactividad
+        />
         {/* Sidebar Menu */}
-        {isMenuOpen && (
-          <Animated.View
-            style={[
-              styles.sidebarMenu,
-              { transform: [{ translateX: slideAnim }] },
-            ]}
-          >
-            <TouchableOpacity
-              onPress={toggleMenu}
-              style={styles.closeButton}
-            ></TouchableOpacity>
-            <View style={styles.sidebarContent}>
-              <Text style={styles.sidebarTitle}>Chats Anteriores</Text>
-              <ScrollView>
-                {previousChats.map((chat, index) => (
-                  <TouchableOpacity key={index} style={styles.chatItem}>
-                    <Text style={styles.chatText}>{chat}</Text>
-                  </TouchableOpacity>
-                ))}
-              </ScrollView>
-            </View>
-          </Animated.View>
-        )}
-
-        <Link style={styles.aboutButton} href="/about">
-          Ir al About
-        </Link>
+        <Animated.View
+          style={[
+            styles.sidebarMenu,
+            { transform: [{ translateX: slideAnim }] },
+          ]}
+          pointerEvents={isMenuOpen ? "auto" : "none"} // Control de la interactividad
+        >
+          <TouchableOpacity
+            onPress={toggleMenu}
+            style={styles.closeButton}
+          ></TouchableOpacity>
+          <View style={styles.sidebarContent}>
+            <Text style={styles.sidebarTitle}>Chats Anteriores</Text>
+            <ScrollView>
+              {previousChats.map((chat, index) => (
+                <TouchableOpacity key={index} style={styles.chatItem}>
+                  <Text style={styles.chatText}>{chat}</Text>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+          </View>
+        </Animated.View>
 
         {/* Chat content */}
         <View style={styles.chatContent}>
@@ -129,7 +115,7 @@ export default function Chat() {
             {/* Right Prompt */}
             <View style={styles.promptRight}>
               <View style={styles.promptBox}>
-                <Text style={styles.promptText}>Ejemplo Prompt</Text>
+                <Text style={styles.promptText}>Idea de Prompt</Text>
                 <Text style={styles.icon}>💡</Text>
               </View>
             </View>
@@ -137,7 +123,7 @@ export default function Chat() {
             {/* Left Prompt */}
             <View style={styles.promptLeft}>
               <View style={styles.promptBox}>
-                <Text style={styles.promptText}>Ejemplo Pormpt</Text>
+                <Text style={styles.promptText}>Idea de Pormpt</Text>
                 <Text style={styles.icon}>💡</Text>
               </View>
             </View>
@@ -168,38 +154,33 @@ export default function Chat() {
 
 const styles = StyleSheet.create({
   logo: {
-    top: Platform.OS === "web" ? "25%" : "10%", // Posición ajustada para que quede en el fondo
-    left: Platform.OS === "web" ? "50%" : "45%",
-    transform: [
-      { translateX: Platform.OS === "web" ? -width * 0.185 : -width * 0.35 },
-    ], // Centrado dinámico
-    width: Platform.OS === "web" ? width * 0.4 : width * 0.85, // Tamaño del logo
-    height: Platform.OS === "web" ? height * 0.4 : height * 0.85,
-    opacity: 0.1, // Transparencia del logo
-  },
-  aboutButton: {
     position: "absolute",
-    top: 16,
-    right: 16,
-    color: "#FFF"
+    width: Platform.OS === "web" ? width * 0.4 : width * 0.85,
+    height: Platform.OS === "web" ? height * 0.4 : height * 0.85,
+    top: Platform.OS === "web" ? "25%" : "0",
+    left: Platform.OS === "web" ? "55%" : "45%",
+    transform: [
+      { translateX: Platform.OS === "web" ? -width * 0.185 : -width * 0.37 },
+    ], // Centrado dinámico
+    opacity: 0.1,
   },
   menuButton: {
     position: "absolute",
     top: 16,
     left: 16,
-    zIndex: 10,
+    zIndex: 6,
     flexDirection: "column",
     justifyContent: "space-between",
   },
   sidebarMenu: {
+    zIndex: 5,
     position: "absolute",
     top: 0,
     left: 0,
     height: "100%",
-    width: 256,
     backgroundColor: "#333",
-    zIndex: 5,
     padding: 16,
+    width: Platform.OS === "web" ? 256 : width * 0.8,
   },
   closeButton: {
     position: "absolute",
@@ -207,7 +188,7 @@ const styles = StyleSheet.create({
     right: 16,
   },
   sidebarContent: {
-    marginTop: 64,
+    marginTop: 50,
   },
   sidebarTitle: {
     color: "white",
@@ -216,28 +197,31 @@ const styles = StyleSheet.create({
     marginBottom: 16,
   },
   overlay: {
+    zIndex: 5,
     position: "absolute",
     top: 0,
     left: 0,
     width: "100%",
     height: "100%",
-    backgroundColor: "rgba(0, 0, 0, 0.45)", // Fondo oscuro semitransparente
-    zIndex: 4,
+    backgroundColor: "rgba(0, 0, 0, 0.55)", // Fondo oscuro semitransparente
   },
   chatItem: {
     padding: 8,
+    marginBottom: 8,
     borderRadius: 8,
     backgroundColor: "#444",
-    marginBottom: 8,
   },
   chatText: {
     color: "white",
   },
   chatContent: {
+    zIndex: Platform.OS === "web" ? 10 : 4,
     flex: 1,
     justifyContent: "flex-end",
+    width: Platform.OS === "web" ? width - 256 : "100%",
+    paddingLeft: Platform.OS === "web" ? 256 : 0,
+    marginLeft: Platform.OS === "web" ? "10%" : "2%",
     padding: 16,
-    width: "100%",
   },
   prompts: {
     marginBottom: 16,
@@ -249,15 +233,15 @@ const styles = StyleSheet.create({
     alignItems: "flex-start", // Alinea el prompt a la izquierda
   },
   promptBox: {
-    backgroundColor: "#333",
-    borderRadius: 20,
-    paddingHorizontal: 12,
-    paddingVertical: 8,
     flexDirection: "row",
     alignItems: "center",
-    marginBottom: 8,
-    maxWidth: "80%", // Limita el ancho máximo al 80% de la pantalla
     flexShrink: 1, // Evita que el cuadro crezca más allá del contenido
+    maxWidth: "80%",
+    paddingHorizontal: 20,
+    paddingVertical: 13,
+    marginBottom: 12,
+    backgroundColor: "#333",
+    borderRadius: 20,
   },
   promptText: {
     color: "#CCC",
@@ -268,11 +252,11 @@ const styles = StyleSheet.create({
   inputContainer: {
     flexDirection: "row",
     alignItems: "center",
-    backgroundColor: "#444",
-    borderRadius: 25,
+    width: "100%",
     paddingHorizontal: 16,
     paddingVertical: 12,
-    width: "100%",
+    borderRadius: 25,
+    backgroundColor: "#444",
   },
   input: {
     flex: 1,
